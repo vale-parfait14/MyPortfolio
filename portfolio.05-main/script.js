d76daaee96f422d864b12d551451a290ff785105
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-10-06 00:55:03 +0000

    Add files via upload

---
+++ portfolio.05-main/script.js
@@ -0,0 +1,9 @@
+//alert('BONJOUR PARFAIT');
+const Par = document.querySelector(".fer")
+      const navLinks = document.querySelector(".menu")
+      Par.addEventListener('click', ()=>{
+        navLinks.classList.toggle('menu-mobile')
+      })
+
+
+      
